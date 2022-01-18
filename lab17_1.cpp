67e6ca29fef064741156b51afdd1c4cdc2d834e2
JjackFrost <nongjj_jay@hotmail.com>
2022-01-18 19:52:16 +0700

    Lab17_1 Completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,51 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *A, int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << A[i];
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *A, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        A[i] = (rand() % 101/100.0);
+    }
+}
+
+void findRowSum(const double *o,double *p,int q,int r)
+{
+	for(int i = 0 ; i < q ; i++)
+	{
+		double j = 0 ;
+	 	for(int k = 0 ; k < r ;k++)
+		{
+			j += *((o + k) + (r * i));
+		}
+		p[i] = j;
+	} 
+}
+
+void findColSum(const double *o,double *p,int q,int r)
+{
+	int i = 0, j = 0;
+	p[j] = 0;
+	for(int m = 0 ; m < r ; m++)
+	{
+		i = m;
+		for(int n = 0 ; n < q ; n++)
+		{
+			p[j] += o[i];
+			i += r;
+		}
+		j++;
+		p[j] = 0;
+	}
+}
+
+
+
